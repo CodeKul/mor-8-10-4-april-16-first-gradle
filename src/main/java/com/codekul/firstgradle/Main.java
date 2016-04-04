b268b6ef473d8e56997b70f7af702a398d8a2b60
aniruddha <aniruddha@aniruddha>
2016-04-04 09:07:39 +0530

    initial commit

---
+++ src/main/java/com/codekul/firstgradle/Main.java
@@ -0,0 +1,12 @@
+package com.codekul.firstgradle;
+
+import org.apache.commons.lang.time.DateUtils;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
